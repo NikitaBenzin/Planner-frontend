de3188f0f6dd612e75c29eea24a6a6d521fc9f7a
NikitaBenzin <nikitory05@gmail.com>
2024-10-09 10:21:48 +0200

    first commit

---
+++ src/app/auth/Auth.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+import { IAuthForm } from '@/types/auth.types'
+
+import { DASHBOARD_PAGES } from '@/config/pages-url.config'
+
+import styles from './Auth.module.scss'
+import { authService } from '@/services/auth.service'
+
+export function Auth() {
+	const { register, handleSubmit, reset } = useForm<IAuthForm>({
+		mode: 'onChange'
+	})
+
+	const [isLoginForm, setIsLoginForm] = useState(false)
+
+	const { push } = useRouter()
+
+	const { mutate } = useMutation({
+		mutationKey: ['auth'],
+		mutationFn: (data: IAuthForm) =>
+			authService.main(isLoginForm ? 'login' : 'register', data),
+		onSuccess() {
+			toast.success('Successfully login!')
+			reset()
+			push(DASHBOARD_PAGES.HOME)
+		}
+	})
+
+	const onSubmit: SubmitHandler<IAuthForm> = data => {
+		mutate(data)
+	}
+
+	return (
+		<div className={styles.wrapper}>
+			<form
+				className={styles.authForm}
+				onSubmit={handleSubmit(onSubmit)}
+			>
+				<div className={styles.buttonsWrapper}></div>
+			</form>
+		</div>
+	)
+}
+// 3:23:00
